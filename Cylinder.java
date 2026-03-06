9b05e40ad59d91c271a9e8ff736cdd28051d986e
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-03-06 16:15:27 +0000

    Completed Cylinder class for inheritance workshop

--- Cylinder.java
+++ Cylinder.java
@@ -2,32 +2,42 @@
  * A Cylinder is a Circle plus a height.
  */
 
-// Task 1a. Edit the definition to make Cylinder a subclass of Circle
-//         
+public class Cylinder extends Circle {
 
-public class Cylinder {
-    
-    // Task 1b. add additional private height attribute (double)
+    private double height;
 
+    public Cylinder() {
+        super();
+        height = 1.0;
+    }
 
-    // Task 2. add code to define the following four constructors
-    // no-arg constructor (set height to 1.0)
-    // 1-arg constructor (set height to the given value)
-    // 2-arg constructor (set height and radius to the given values)
-    // 3-arg constructor (set height, radius, and color to the given values)
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+    }
 
+    public double getHeight() {
+        return height;
+    }
 
-    
-    // Task 3. add getter and setter for height
-    // (methods for radius and color are inherited)
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
+    public double getVolume() {
+        return super.getArea() * height;
+    }
 
-    // Task 4. add code for method getVolume()
-    // make use of superclass' getArea() method
+    @Override
+    public double getArea() {
 
- 
-    // Task 5. Override toString() method to describe itself
-    // (output format should be in line with format: Cylinder[Circle[...],height=X.XX]
+        double baseArea = super.getArea();
+        double circumference = 2 * Math.PI * getRadius();
 
-    
- }
+        return 2 * baseArea + circumference * height;
+    }
+
+    public double getBaseArea() {
+        return super.getArea();
+    }
+}
